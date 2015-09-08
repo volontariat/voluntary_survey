7e8edb6c76bddbb326c19ec5acb8e99a9c2c7be9
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-09-08 12:40:59 +0200

    Task is orderable.

--- app/models/product/survey/task.rb
+++ app/models/product/survey/task.rb
@@ -1,9 +1,11 @@
 class Product::Survey::Task < ::Task
+  include Mongoid::Orderable
+  
   embedded_in :page, class_name: 'Product::Survey::Page', inverse_of: :tasks
   embeds_many :options, class_name: 'Product::Survey::InputOption', inverse_of: :input
   has_many :results, class_name: 'Product::Survey::Result', foreign_key: 'task_id', dependent: :destroy
   
-  field :position, type: Integer
+  orderable
   
   attr_accessible :position
   
