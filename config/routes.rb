b810d38eb6b81cd9df3632d13e7a4458964e767c
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-09-09 17:45:18 +0200

    refs #2 routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :voluntary, path: 'api', module: 'voluntary/api', defaults: {format: 'json'} do
     namespace :v1 do
-      resources :survey_pages, only: [:index, :create, :show, :update, :destroy]
+      resources :survey_pages, only: [:create, :show, :update, :destroy]
+      resources :survey_tasks, only: [:create, :show, :update, :destroy]
     end
   end
 end
