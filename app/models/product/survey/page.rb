3161d53d138a726529d046e08cd27a661a4a788a
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-09-10 12:15:23 +0200

    refs #2 page does not embed tasks anymore and adding custom fields to survey task model.

--- app/models/product/survey/page.rb
+++ app/models/product/survey/page.rb
@@ -5,7 +5,7 @@ class Product::Survey::Page
   include Mongoid::Orderable
   
   embedded_in :story, class_name: 'Product::Survey::Story', inverse_of: :pages
-  embeds_many :tasks, class_name: 'Product::Survey::Task', inverse_of: :page
+  has_many :tasks, class_name: 'Product::Survey::Task', inverse_of: :page
   
   field :name, type: String
   field :text, type: String
--- app/models/product/survey/task.rb
+++ app/models/product/survey/task.rb
@@ -1,14 +1,20 @@
 class Product::Survey::Task < ::Task
   include Mongoid::Orderable
   
-  embedded_in :page, class_name: 'Product::Survey::Page', inverse_of: :tasks
+  belongs_to :page, class_name: 'Product::Survey::Page', inverse_of: :tasks
+  
   embeds_many :options, class_name: 'Product::Survey::InputOption', inverse_of: :input
   has_many :results, class_name: 'Product::Survey::Result', foreign_key: 'task_id', dependent: :destroy
   
+  field :answer_type, type: String
+  
+  validates :name, presence: true, uniqueness: { scope: :story_id }
+  validates :answer_type, presence: true
+  
   orderable
   
-  attr_accessible :position
-  
+  attr_accessible :position, :answer_type
+
   protected
   
   def with_offeror; false; end
